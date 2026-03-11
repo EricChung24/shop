138f0f07f140d1b90c024aaacbc0fc0e330e4d34
EricChung <lf2net67983@gmail.com>
2026-03-11 23:55:52 +0800

    變更路徑

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -3,10 +3,16 @@ function App() {
 
   return (
     <>
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold">Welcome to Vite + React</h1>
-        <p className="mt-4">This is a simple starter template.</p>
+         <main className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="rounded-2xl bg-white p-8 shadow-lg text-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          腿 已成功接上
+        </h1>
+        <p className="mt-4 text-gray-600">
+          這是你目前的 Vite + React 專案
+        </p>
       </div>
+    </main>
     </>
   )
 }
