26c22a02eebfbfd25c4b0c7b966c1f1e069d2cea
EricChung <lf2net67983@gmail.com>
2026-03-11 22:08:36 +0800

    first commit

---
+++ shop/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className="text-4xl font-bold">Welcome to Vite + React</h1>
+        <p className="mt-4">This is a simple starter template.</p>
+      </div>
+    </>
+  )
+}
+
+export default  App
